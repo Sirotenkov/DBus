bc8d95b99975250394408e3a921d03318191dd6e
Алексей <aleksej.sirotenkov@simaster.ru>
2024-10-28 21:37:20 +0300

    Добавлены переводы

---
+++ translations/ru.template.DBus-ru.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru">
+<context>
+    <name>DefaultCoverPage</name>
+    <message>
+        <location filename="../qml/cover/DefaultCoverPage.qml" line="10"/>
+        <source>DBus</source>
+        <translation>Учебное приложение для ОС Аврора</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="11"/>
+        <source>DBus-service</source>
+        <translation>DBus-служба</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="20"/>
+        <source>Send a request</source>
+        <translation>Отправить запрос</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/ru.template.DBus.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+<context>
+    <name>DefaultCoverPage</name>
+    <message>
+        <location filename="../qml/cover/DefaultCoverPage.qml" line="10"/>
+        <source>DBus</source>
+        <translation>DBus</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="11"/>
+        <source>DBus-service</source>
+        <translation>DBus-service</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="20"/>
+        <source>Send a request</source>
+        <translation>Send a request</translation>
+    </message>
+</context>
+</TS>
